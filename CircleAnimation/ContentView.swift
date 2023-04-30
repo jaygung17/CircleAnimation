dcde11d2cabb8dc1c49c89c9e7b53b8ddd707066
Novan Agung Waskito <68317534+jaygung17@users.noreply.github.com>
2023-04-30 19:13:50 +0700

    CircleAnimationWithSwiftUI

--- CircleAnimation/ContentView.swift
+++ CircleAnimation/ContentView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var fill: CGFloat = 0.0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color(UIColor(red: 11/255.0, green: 15/255.0, blue: 128/255.0, alpha: 1))
+            ZStack{
+                
+                //Track Circle
+                Circle()
+                    .stroke(Color.white.opacity(0.3),
+                            style: StrokeStyle(lineWidth: 30))
+                
+                //Animation Circle
+                Circle()
+                    .trim(from: 0, to: self.fill)
+                    .stroke(Color.pink,
+                            style: StrokeStyle(lineWidth: 30))
+                    .rotationEffect(.init(degrees: -90))
+                    .animation(Animation.linear(duration: 450.0))
+                Text("PinkDotSG \(Int(self.fill * 1.0)) %")
+                    .foregroundColor(Color.pink)
+                    .font(.system(size: 32))
+            }
+            .padding(50)
         }
-        .padding()
+        .onTapGesture {
+            //Increment method
+            for x in 1...100 {
+                DispatchQueue.main.asyncAfter(deadline: .now()+TimeInterval(x/30)) {
+                    self.fill += 1
+                }
+            }
+        }
+        
     }
 }
 
