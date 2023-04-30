44900d2189758c29f0790eba26a588e857f82f70
Novan Agung Waskito <68317534+jaygung17@users.noreply.github.com>
2023-04-30 17:34:11 +0700

    Initial Commit

---
+++ CircleAnimation/CircleAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircleAnimationApp.swift
+//  CircleAnimation
+//
+//  Created by Novan Agung Waskito on 30/4/23.
+//
+
+import SwiftUI
+
+@main
+struct CircleAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CircleAnimation/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CircleAnimation
+//
+//  Created by Novan Agung Waskito on 30/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
